228bd57300bde3c51ff945c1a50f2f7c87652f35
reiden <pattuzzo@pm.me>
2025-10-22 03:07:01 -0300

    add links to each tech on technology section

--- src/components/card/card-tech.tsx
+++ src/components/card/card-tech.tsx
@@ -1,17 +1,18 @@
 interface CardTechProps {
   title: string;
   description: string;
+  href: string;
   children: React.ReactNode;
 }
 
 export default function CardTech(props: CardTechProps) {
   return (
-    <div className="w-full flex justify-between items-center gap-4 max-w-md border-1 border-slate-700 rounded-md p-4 bg-slate-900">
+    <a className="w-full flex justify-between items-center gap-4 max-w-md border-1 border-slate-700 rounded-md p-4 bg-slate-900 hover:bg-slate-700" href={props.href} target="_blank">
       <div className="flex justify-center items-center w-16 h-16 rounded-md bg-slate-800">{props.children}</div>
       <div className="w-5/6 flex flex-col justify-between">
         <h5>{props.title}</h5>
         <p className="text-sm text-slate-400">{props.description}</p>
       </div>
-    </div>
+    </a>
   )
 }
--- src/views/sections/techs-menu.tsx
+++ src/views/sections/techs-menu.tsx
@@ -36,30 +36,35 @@ export default function TechsMenu() {
           <CardTech
             title={t("home:technologies.sections.front-end.items.astro.title")}
             description={t("home:technologies.sections.front-end.items.astro.description")}
+            href={t("home:technologies.sections.front-end.items.astro.href")}
           >
             <img src={Astro.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.front-end.items.nextjs.title")}
             description={t("home:technologies.sections.front-end.items.nextjs.description")}
+            href={t("home:technologies.sections.front-end.items.nextjs.href")}
           >
             <img src={Nextjs.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.front-end.items.solidjs.title")}
             description={t("home:technologies.sections.front-end.items.solidjs.description")}
+            href={t("home:technologies.sections.front-end.items.solidjs.href")}
           >
             <img src={Solidjs.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.front-end.items.react.title")}
             description={t("home:technologies.sections.front-end.items.react.description")}
+            href={t("home:technologies.sections.front-end.items.react.href")}
           >
             <img src={ReactIcon.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.front-end.items.tailwind.title")}
             description={t("home:technologies.sections.front-end.items.tailwind.description")}
+            href={t("home:technologies.sections.front-end.items.tailwind.href")}
           >
             <img src={Tailwind.src} className="h-8 w-8" />
           </CardTech>
@@ -68,24 +73,28 @@ export default function TechsMenu() {
           <CardTech
             title={t("home:technologies.sections.back-end.items.nestjs.title")}
             description={t("home:technologies.sections.back-end.items.nestjs.description")}
+            href={t("home:technologies.sections.back-end.items.nestjs.href")}
           >
             <img src={Nestjs.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.back-end.items.express.title")}
             description={t("home:technologies.sections.back-end.items.express.description")}
+            href={t("home:technologies.sections.back-end.items.express.href")}
           >
             <img src={Express.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.back-end.items.deno.title")}
             description={t("home:technologies.sections.back-end.items.deno.description")}
+            href={t("home:technologies.sections.back-end.items.deno.href")}
           >
             <img src={Deno.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.back-end.items.node.title")}
             description={t("home:technologies.sections.back-end.items.node.description")}
+            href={t("home:technologies.sections.back-end.items.node.href")}
           >
             <img src={Nodejs.src} className="h-8 w-8" />
           </CardTech>
@@ -94,24 +103,28 @@ export default function TechsMenu() {
           <CardTech
             title={t("home:technologies.sections.database.items.drizzle.title")}
             description={t("home:technologies.sections.database.items.drizzle.description")}
+            href={t("home:technologies.sections.database.items.drizzle.href")}
           >
             <img src={Drizzle.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.database.items.prisma.title")}
             description={t("home:technologies.sections.database.items.prisma.description")}
+            href={t("home:technologies.sections.database.items.prisma.href")}
           >
             <img src={Prisma.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.database.items.postgres.title")}
             description={t("home:technologies.sections.database.items.postgres.description")}
+            href={t("home:technologies.sections.database.items.postgres.href")}
           >
             <img src={Postgres.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.database.items.mongodb.title")}
             description={t("home:technologies.sections.database.items.mongodb.description")}
+            href={t("home:technologies.sections.database.items.mongodb.href")}
           >
             <img src={MongoDB.src} className="h-8 w-8" />
           </CardTech>
@@ -120,12 +133,14 @@ export default function TechsMenu() {
           <CardTech
             title={t("home:technologies.sections.tests.items.vitest.title")}
             description={t("home:technologies.sections.tests.items.vitest.description")}
+            href={t("home:technologies.sections.tests.items.vitest.href")}
           >
             <img src={Vitest.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.tests.items.jest.title")}
             description={t("home:technologies.sections.tests.items.jest.description")}
+            href={t("home:technologies.sections.tests.items.jest.href")}
           >
             <img src={Jest.src} className="h-8 w-8" />
           </CardTech>
@@ -134,12 +149,14 @@ export default function TechsMenu() {
           <CardTech
             title={t("home:technologies.sections.tools.items.git.title")}
             description={t("home:technologies.sections.tools.items.git.description")}
+            href={t("home:technologies.sections.tools.items.git.href")}
           >
             <img src={Git.src} className="h-8 w-8" />
           </CardTech>
           <CardTech
             title={t("home:technologies.sections.tools.items.docker.title")}
             description={t("home:technologies.sections.tools.items.docker.description")}
+            href={t("home:technologies.sections.tools.items.docker.href")}
           >
             <img src={Docker.src} className="h-8 w-8" />
           </CardTech>
